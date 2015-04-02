70af7859ad60a20882e733971f4e381d681a5f61
Spidergost <thespidergost@gmail.com>
2015-04-02 16:02:23 +0200

    Passage en V2.3 de index.php
    Ajout des 5 autres pages.

--- index.php
+++ index.php
@@ -9,28 +9,27 @@
 		<div class="liens">
 			
 			<div class="page_1">
-				<a href="insertion-stagiaire.php"> Page 1 </a>
-
+				<a href="insertion-stagiaire.php"> Insérer un stagiaire </a>
 			</div>
             
 			<div class="page_2">
-			
+				<a href="affichage-stagiaires.php"> Afficher les stagiaires </a>
 			</div>
-            
+
 			<div class="page_3">
-			
+				<a href="insertion-stagiaire.php"> Supprimer un stagiaire </a>			
 			</div>
 			
 			<div class="page_4">
-			
+				<a href="insertion-stagiaire.php"> Modifier les informations d'un stagiaire </a>			
 			</div>
 			
 			<div class="page_5">
-			
+				<a href="insertion-stagiaire.php"> Liste des formations, leurs formateurs, <br/> leurs salles avec les dates </a>			
 			</div>
 			
 			<div class="page_6">
-			
+				<a href="insertion-stagiaire.php"> Page 6 </a>			
 			</div>
 			
 		</div>
