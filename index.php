bbd418a8a85f9268d90bb80771650c231ff67c47
Spidergost <thespidergost@gmail.com>
2015-04-02 15:21:48 +0200

    Update off index.php Passage en V2.2
    V2.2

--- index.php
+++ index.php
@@ -7,12 +7,31 @@
 
     <body>
 		<div class="liens">
-			<div class="page_1"></div>
-            <div class="page_2"></div>
-            <div class="page_3"></div>
-			<div class="page_4"></div>
-			<div class="page_5"></div>
-			<div class="page_6"></div>
+			
+			<div class="page_1">
+				<a href="insertion-stagiaire.php"> Page 1 </a>
+
+			</div>
+            
+			<div class="page_2">
+			
+			</div>
+            
+			<div class="page_3">
+			
+			</div>
+			
+			<div class="page_4">
+			
+			</div>
+			
+			<div class="page_5">
+			
+			</div>
+			
+			<div class="page_6">
+			
+			</div>
 			
 		</div>
 		
