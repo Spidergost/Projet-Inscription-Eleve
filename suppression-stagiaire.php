802d0fdd4430ebd7626dcf65431165fbb98049c3
watira <laura.serre63@gmail.com>
2015-04-09 14:59:07 +0200

    MAJ suppression.php

--- suppression-stagiaire.php
+++ suppression-stagiaire.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>Suppression Stagiaire</title>
+		<link rel="stylesheet" type="text/css" href="css/affichage-stagiaires.css" media="screen" />
     </head>
 
     <body>
@@ -10,7 +10,61 @@
 			<a > <h1> Suppression de Stagiaire</h1> </a>
 		</div>
 		
-		<h1><?php include('affichage-stagiaires.php'); ?></h1>
+				<?php
+			try {
+		        // Connexion à la base de données
+		        $db = new PDO('mysql:host=localhost;dbname=formation', 'root', '');
+		        // prise en charge de l'utf-8
+		        $db->exec("SET CHARACTER SET utf8");
+		        echo "<p>Connexion à la base réussi.</p> <br/>";
+
+		        $requeteAffichage = $db->query('SELECT STAGIAIRE.ID AS id_stagiaire, STAGIAIRE.NOM AS nom_stagiaire, STAGIAIRE.PRENOM AS prenom_stagiaire, TYPE_FORMATION.LIBELLE AS formation, NATIONALITE.LIBELLE AS nationalite, FORMATEUR.NOM AS nom_formateur, FORMATEUR.PRENOM AS prenom_formateur, SALLE.LIBELLE AS salle, STAGIAIRE_FORMATEUR.DATE_DEBUT AS debut_formation,STAGIAIRE_FORMATEUR.DATE_FIN AS fin_formation
+												FROM STAGIAIRE 
+												LEFT JOIN TYPE_FORMATION 
+												ON STAGIAIRE.ID_TYPE_FORMATION = TYPE_FORMATION.ID_TYPE_FORMATION
+												LEFT JOIN NATIONALITE 
+												ON STAGIAIRE.ID_NATIONALITE = NATIONALITE.ID_NATIONALITE
+												LEFT JOIN STAGIAIRE_FORMATEUR
+												ON STAGIAIRE.ID = STAGIAIRE_FORMATEUR.ID
+												LEFT JOIN FORMATEUR
+												ON STAGIAIRE_FORMATEUR.ID_FORMATEUR = FORMATEUR.ID_FORMATEUR
+												LEFT JOIN SALLE
+												ON FORMATEUR.ID_SALLE = SALLE.ID_SALLE
+												ORDER BY STAGIAIRE.ID;');
+						
+
+		        //En cas de reussite
+				$nbStagiaire=$requeteAffichage->rowCount();
+				$tabResult=$requeteAffichage->fetchAll(PDO::FETCH_ASSOC);
+				echo "<p>Il y a $nbStagiaire stagiaires(s)</p>";
+				$titres=array_keys($tabResult[0]);
+				echo "<table border=\"1\"><tr>";
+
+				//Affichage des titres du tableau
+				foreach($titres as $nomcol)
+				{
+					echo "<th>", htmlentities($nomcol), "</th>";
+				}
+				echo "</tr>";
+
+				//Affichage des lignes de données
+				for ($i=0; $i<$nbStagiaire; $i++)
+				{
+					echo "<tr>";
+					foreach ($tabResult[$i] as $valeur)
+					{
+						echo "<td> $valeur </td>";
+					}
+					echo "</tr>";
+				}
+				echo "</table class='styletableau' border=\"1\"> <br />";
+
+		    }
+		    catch (PDOException $e) {
+		    	echo "Erreur : " . $e->getMessage() . "<br/>";
+		    	die();
+		    }
+		?>
 				
     	<form method="post" action="traitement-suppression.php" class="formulaire">
 			<p>ID :	<input type="text" name="id" /> <br /> </p>
@@ -27,6 +81,6 @@
 				}
 			}	
 		?>
-    
+    <a href="index.php">Retour au menu</a>
     </body>
 </html>
