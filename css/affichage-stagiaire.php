ae7df3c217191849f2ae14404805b3f9a93ea477
MaitreDuMonde <sarah.rouibi@voila.fr>
2015-04-09 11:27:20 +0200

    Create affichage-stagiaire.php
    J'ai rajouté des balises tout au début, avant le php, afin que la page soit beaucoup plus jolie.

---
+++ css/affichage-stagiaire.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="css/affichage-stagiaires.css" media="screen" />
+        <title>Liste des stagiaires</title>
+    </head>
+
+    <body>
+        <div class="titre">
+    	<h1>Liste des stagiaires : </h1>
+        </div>
+		<?php
+			try {
+		        // Connexion à la base de données
+		        $db = new PDO('mysql:host=localhost;dbname=formation', 'root', '');
+		        // prise en charge de l'utf-8
+		        $db->exec("SET CHARACTER SET utf8");
+		        echo "<p>Connexion à la base réussi.</p> <br/>";
+
+		        $requeteAffichage = $db->query('SELECT ID, NOM, PRENOM, TYPE_FORMATION.LIBELLE AS Formation, NATIONALITE.LIBELLE AS Nationalite
+		        		FROM STAGIAIRE
+		        		LEFT JOIN TYPE_FORMATION
+		        		ON STAGIAIRE.ID_TYPE_FORMATION = TYPE_FORMATION.ID_TYPE_FORMATION
+		        		LEFT JOIN NATIONALITE
+		        		ON STAGIAIRE.ID_NATIONALITE = NATIONALITE.ID_NATIONALITE
+		        		ORDER BY ID');
+
+		        //En cas de reussite
+				$nbStagiaire=$requeteAffichage->rowCount();
+				$tabResult=$requeteAffichage->fetchAll(PDO::FETCH_ASSOC);
+				echo "<p>Il y a $nbStagiaire stagiaires(s)</p>";
+				$titres=array_keys($tabResult[0]);
+				echo "<table border=\"1\"><tr>";
+
+				//Affichage des titres du tableau
+				foreach($titres as $nomcol)
+				{
+					echo "<th>", htmlentities($nomcol), "</th>";
+				}
+				echo "</tr>";
+
+				//Affichage des lignes de données
+				for ($i=0; $i<$nbStagiaire; $i++)
+				{
+					echo "<tr>";
+					foreach ($tabResult[$i] as $valeur)
+					{
+						echo "<td> $valeur </td>";
+					}
+					echo "</tr>";
+				}
+				echo "</table class='styletableau' border=\"1\"> <br />";
+
+		    }
+		    catch (PDOException $e) {
+		    	echo "Erreur : " . $e->getMessage() . "<br/>";
+		    	die();
+		    }
+		?>
+
+		<a href="index.php">Retour au menu</a>
+	</body>
+</html>
