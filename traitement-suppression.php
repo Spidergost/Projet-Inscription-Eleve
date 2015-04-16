350d680702425f459a7df35ebf2fa43dc2fc8c5d
watira <laura.serre63@gmail.com>
2015-04-16 13:18:13 +0200

    traitement-suppression MAJ finale

--- traitement-suppression.php
+++ traitement-suppression.php
@@ -12,14 +12,20 @@
             $db->exec("SET CHARACTER SET utf8");
             echo "Connexion à la base réussi. <br/>";
             
+			/* 1ère requete */
+			
             // Requête SQL préparé
-            $requeteSuppression = $db->prepare('DELETE FROM `STAGIAIRE` WHERE ((`ID` = :id));');
-
+            $requeteSuppression = $db->prepare('DELETE FROM STAGIAIRE WHERE ID = :id');
             // On remplie les paramètres
             $requeteSuppression->bindParam(':id', $id, PDO::PARAM_INT, 2);
-			
             // On l'éxecute
             $requeteSuppression->execute();
+			
+			/* 2ème requete */
+			$requeteSuppression2 = $db->prepare('DELETE FROM STAGIAIRE_FORMATEUR WHERE ID = :id');
+			$requeteSuppression2->bindParam( ':id', $id, PDO::PARAM_INT, 2);
+			$requeteSuppression2->execute();
+			
             echo "Stagiaire supprimé avec succès !";
 
             // Redirection
