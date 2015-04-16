5ac65506191c9bda39ffbb7e760d3192760b338d
MaitreDuMonde <sarah.rouibi@voila.fr>
2015-04-16 09:37:09 +0200

    CSS+PHP V1
    Voici le CSS propre à la page "listing.php". L'HTML a également était
    modifié dans le page planning.php afin que cela soit plus propre.

--- listing.php
+++ listing.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8" />
-		<link rel="stylesheet" type="text/css" href="css/insertion-stagiaire.css" media="screen" />
+		<link rel="stylesheet" type="text/css" href="css/listing.css" media="screen" />
         <title>Insertion de stagiaire</title>
     </head>
 
@@ -13,37 +13,41 @@
     	</div>
 
     	<form method="post" action="traitement-listing.php" class="formulaire">
-		
-	    	<p>Nom :	<br /><input type="text" name="nom" /> <br /> </p>
-		  	<p>Prénom : <br /><input type="text" name="prenom" /> <br /> </p>
+
+	    	<p>Nom :	<input type="text" name="nom" /> <br /> </p>
+		  	<p>Prénom : <input type="text" name="prenom" /> <br /> </p>
 	      	<p>
-			
-				Nationalité : 
+
+				Nationalité :
 				<select name="nationalite">
 						<option selected="selected" value="1"> Français</option>
 						<option value="2"> Anglais</option>
 						<option value="3"> Allemand</option>
 						<option value="4"> Russe</option>
-			    </select>  
+			    </select>
 	      	</p>
-	      
+
 	      	<p>
-				Type de la formation : 
+				Type de la formation :
 				<select name="formation">
 						<option selected="selected" value="1"> Web designer</option>
 						<option value="2"> Développpeur</option>
-			    </select>  
+			    </select>
 	      	</p>
-			<input type="hidden" name="envoi" value="yes">			
+			<input type="hidden" name="envoi" value="yes">
 			<p>
-				 formateurs par date :<br />
+				 <b><u>Formateurs par date</u></b> :<br/>
+				 <br/>
 				<input type="checkbox" name="option[]" value="1"/> <label> Robert Dupond dans la salle 101, début: <input type="text" name="date_debut" value="28/09/2011"/>  fin: <input type="text" name="date_fin" value="28/03/2012" /></label><br />
+				<br/>
 				<input type="checkbox" name="option[]" value="2"/> <label> Jean Martin dans la salle 102, début: <input type="text" name="date_debut" value="28/09/2011"/>  fin: <input type="text" name="date_fin" value="28/03/2012"/></label><br />
+				<br/>
 				<input type="checkbox" name="option[]" value="3"/> <label> Paul Durand dans la salle 201, début: <input type="text" name="date_debut" value="28/09/2011"/>  fin: <input type="text" name="date_fin" value="28/03/2012"/></label><br />
+				<br/>
 				<input type="checkbox" name="option[]" value="4"/> <label> Alain Duval dans la salle 202, début: <input type="text" name="date_debut" value="28/09/2011"/>  fin: <input type="text" name="date_fin" value="28/03/2012"/></label>
 			</p>
 			<p>
-				<input type="submit" value="envoyer" />
+				<input type="submit" value="Envoyer" />
 	     	</p>
     	</form>
 
@@ -55,8 +59,8 @@
 				if ($_GET['erreur']==1) {
 					echo "<script>alert('Veuillez remplir le formulaire en entier.')</script>";
 				}
-			}	
+			}
 		?>
-    
+
     </body>
 </html>
