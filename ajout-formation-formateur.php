5b8192a254292ac4bdf608b43b8b896f95c1425a
Spidergost <thespidergost@gmail.com>
2015-04-20 10:38:54 +0200

    Ajout formation formateur

--- ajout-formation-formateur.php
+++ ajout-formation-formateur.php
@@ -1,1 +1,25 @@
-﻿
+﻿<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="css/affichage-stagiaires.css" media="screen" />
+        <title>Ajout de formations et de formateurs</title>
+    </head>
+
+    <body>
+	<div class="titre">
+    	<h1>Liste des formateurs : </h1>
+	</div>
+		
+	<div class="tableau">
+		<?php include('req-affichage-forma.php'); ?>
+	</div>
+	
+        </br>
+		<a href="index.php">Retour au menu</a>
+
+	</body>
+</html>
+
+
+ftp://touhou-online.net/Jeux/Touhou%2009%20-%20Phantasmagoria%20of%20Flower%20View.zip
