31ad9cba36dcd15a9c63727f11e34087112edb11
CLitoris <ju-d-orange@live.fr>
2015-04-23 11:22:15 +0200

    comit assemblage.php

--- assemblage.php
+++ assemblage.php
@@ -1,8 +1,17 @@
-﻿<!DOCTYPE html>
+﻿<?php
+session_start();
+$idFormation = isset($_POST['idFormation']);
+$option = isset($_POST['option']);
+$id_formateur = $option;
+$nom = isset($_POST['nom']);
+$prenom = isset($_POST['prenom']);
+$idNationalite = isset($_POST['idNationalite']);
+?>
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8" />
-		<link rel="stylesheet" type="text/css" href="css/assemblage.css" media="screen" />
+		<link rel="stylesheet" type="text/css" href="css/insertion-stagiaire.css" media="screen" />
         <title>Insertion de stagiaire</title>
     </head>
 
@@ -14,7 +23,7 @@
 					$db = new PDO('mysql:host=localhost;dbname=formation', 'root', '');
 					// prise en charge de l'utf-8
 					$db->exec("SET CHARACTER SET utf8");
-					//echo "Connexion à la base réussi. <br/>";
+					echo "Connexion à la base réussi. <br/>";
 				}
 				catch (PDOException $e) {
 					echo "Erreur : " . $e->getMessage() . "<br/>";
@@ -26,10 +35,10 @@
     		<h1> Listing en fonction des formations  </h1>
     	</div>
 
-    	<form method="post" action="assemblage.php" class="formulaire">
-
-	    	<p>Nom : <input type="text" name="nom" /> <br /> </p>
-		  	<p>Prénom : <input type="text" name="prenom" /> <br /> </p>
+    	<form method="post" action="assemblageV2test.php" class="formulaire">
+	
+	    	<p>Nom :	<br /><input type="text" name="nom" /> <br /> </p>
+		  	<p>Prénom : <br /><input type="text" name="prenom" /> <br /> </p>
 	      	<?php
 		        // NATIONALITE
 		    	// Requête pour avoir le nombre de nationalite
@@ -40,7 +49,7 @@
 		        echo "<p>$nbNationalite nationalité(s) : ";
 
 		        // Affichage des nationalites
-		        echo "<select name=\"nationalite\">";
+		        echo "<select name=\"idNationalite\">";
 		        for ($i=1; $i < ($nbNationalite+1) ; $i++) {
 		        	// Requête SQL préparé
 		        	$requeteNationalite = $db->prepare('SELECT * FROM NATIONALITE WHERE ID_NATIONALITE=:id');
@@ -48,7 +57,7 @@
 		            $requeteNationalite->bindParam(':id', $i, PDO::PARAM_INT, 2);
 		            // On l'éxécute
 		            $requeteNationalite->execute();
-		            // On fetch Right !
+		            // On fetch Rigght !
 		        	$valeurNationalite = $requeteNationalite->fetch();
 		        	$idNationalite = $valeurNationalite['ID_NATIONALITE'];
 		        	$libelleNationalite = $valeurNationalite['LIBELLE'];
@@ -56,9 +65,9 @@
 		        }
 		        echo "</select> </p>";
     		?>
-
-           <font size="2"><a href="ajout-nationalite.php"> Souhaitez-vous ajouter une nationalité ? </a></font>
-
+			
+           <font size="2"><a href="ajout-nationalite.php"> Souhaitez-vous ajouter une nationalité ? </a></font>			
+		   
     		<?php
 		        // FORMATION
 		    	// Requête pour avoir le nombre de formation
@@ -68,7 +77,7 @@
 		        echo "<p> $nbFormation formation(s) : ";
 
 		        // Affichage des formations
-		        echo "<select name=\"formation\">";
+		        echo "<select name=\"idFormation\">";
 		        for ($i=1; $i < ($nbFormation+1) ; $i++) {
 		        	// Requête SQL préparé
 		        	$requeteFormation = $db->prepare('SELECT * FROM TYPE_FORMATION WHERE ID_TYPE_FORMATION=:id');
@@ -84,91 +93,116 @@
 		        }
 		        echo "</select> </p>";
     		?>
+			
+			<?php
+			// FORMATEUR
+		    	// Requête pour avoir le nombre de formateur
+		    	$requeteNbFormateur = $db->query('SELECT COUNT(ID_FORMATEUR) FROM FORMATEUR');
+		        $valeurNbFormateur = $requeteNbFormateur->fetch();
+		        $nbFormateur = $valeurNbFormateur['COUNT(ID_FORMATEUR)'];
+		        echo "<p> $nbFormateur formateur(s) : ";
+				
+				//affichage des formateur
+				echo "<select name='option'>";
+				for ($i=1; $i < ($nbFormateur+1) ; $i++) {
+					// Requête SQL préparé
+					$requeteFormateur = $db->prepare('SELECT * FROM FORMATEUR WHERE ID_FORMATEUR=:id');
+					// On remplie les paramêtres
+					$requeteFormateur->bindParam(':id', $i, PDO::PARAM_INT, 2);
+					// On l'éxécute
+					$requeteFormateur->execute();
+					// On fetch Rigght !
+					$valeurFormateur = $requeteFormateur->fetch();
+					$idFormateur = $valeurFormateur['ID_FORMATEUR'];
+					$libelleFormateur = $valeurFormateur['NOM'];
+					$prenom = $valeurFormateur['PRENOM'];
+					$salle = $valeurFormateur['ID_SALLE'];
+					echo "<option value='$idFormateur'> $libelleFormateur $prenom - salle: $salle</option><br />";
+				}
+				echo "</select></p>";
+				?>
+				<font size="2"><a href="ajout-formation-formateur-assemblage.php"> Souhaitez-vous ajouter un formateur et une formation ? </a></font>
+				<br />
+				
+				formateurs par date :<br /><br />
 
-				 <b><u>Formateurs par date</u> : </b><br /><br />
-				<input type="checkbox" name="option[]" value="1"/> Robert Dupond dans la salle 101,<br />
-				<input type="checkbox" name="option[]" value="2"/> Jean Martin dans la salle 102, <br />
-				<input type="checkbox" name="option[]" value="3"/> Paul Durand dans la salle 201, <br />
-				<input type="checkbox" name="option[]" value="4"/> Alain Duval dans la salle 202,
-			</p>
-
-
-			    <!-- <input type="hidden" name="bouton" value="ok" /> -->
-			</p>
-				<input type="hidden" name="envoyer" value="Afficher les formations">
-			<p>
-			<p>
-				<input type="submit" name="envoyer" value="Afficher les formations" />
+		
+			<p>	
+				<input type="submit" name="envoyer" value="afficher les formations" />
 	     	</p>
-				<input type="submit" name="envoyer" value="Envoyer" />
+				<input type="submit" name="envoyer" value="envoyerr" />
     	</form>
-
-		<?php
+		
+		<?php		
 		if($_POST['envoyer']=='afficher les formations'){
+					$idFormation = $_POST['idFormation'];
 					//lecture du tableau option[]
 					$option = $_POST['option'];
-					$id_formateur = implode(', ',$option);
+					//$id_formateur = implode(', ',$option);
 					//echo '<p>options:<br><br>'.$id_formateur.'</p>';
+					$id_formateur = $option; 
 					// Dates début
-					// Requête pour avoir le nombre de dates de début
-					$requeteNbDates_debut = $db->query('SELECT COUNT(DATE_DEBUT) FROM STAGIAIRE_FORMATEUR WHERE ID_FORMATEUR=' . $id_formateur . '');
-					$valeurNbDates_debut = $requeteNbDates_debut->fetch();
-					$nbDates_debut = $valeurNbDates_debut['COUNT(DATE_DEBUT)'];
-					echo "<p>$nbDates_debut dates début : ";
+					if($idFormation!='')
+					{
+					
+						// Requête pour avoir le nombre de dates de début
+						$requeteNbDates_debut = $db->query('SELECT COUNT(DATE_DEBUT_FORMATION) FROM TYPE_FORMATION_FORMATEUR WHERE ID_FORMATEUR=' . $id_formateur . ' AND ID_TYPE_FORMATION=' . $idFormation . '');
+						$valeurNbDates_debut = $requeteNbDates_debut->fetch();
+						$nbDates_debut = $valeurNbDates_debut['COUNT(DATE_DEBUT_FORMATION)'];
+						echo "<p>$nbDates_debut dates début : ";
 
 						// Affichage des dates début
+						echo "<select name='date_debut'>";
 						// Requête SQL préparé
-						$requeteDates_debut = $db->prepare('SELECT * FROM STAGIAIRE_FORMATEUR WHERE ID_FORMATEUR=' . $id_formateur . '');
+						$requeteDates_debut = $db->prepare('SELECT * FROM STAGIAIRE_FORMATEUR LEFT JOIN TYPE_FORMATION_FORMATEUR ON STAGIAIRE_FORMATEUR.ID_FORMATEUR = TYPE_FORMATION_FORMATEUR.ID_FORMATEUR WHERE ID_TYPE_FORMATION=' . $idFormation . ' AND STAGIAIRE_FORMATEUR.ID_FORMATEUR=' . $id_formateur . '');
 						// On remplie les paramètres
 						$requeteDates_debut->bindParam(':id_formateur', $i, PDO::PARAM_STR, 20);
 						// On l'éxécute
 						$requeteDates_debut->execute();
 						// On fetch Rigght !
 						$valeurDate_debut = $requeteDates_debut->fetchAll();
-						echo "<select name=\"dates_debut\">";
 						foreach ($valeurDate_debut as $value){
-
+										
 							$date_debut = $value['DATE_DEBUT'];
-							echo "<option value=\"$date_debut\"> $date_debut </option>"; echo "<br />";
-
+							 echo "<option value='$date_debut'> $date_debut</option><br />";										
 						}
-					echo "</select>";
+						echo "</select></p>";
+						//Date fin
+						//requête pour avoir le nombre de dates à la fin
+						$requeteNbDates_fin = $db->query('SELECT COUNT(DATE_FIN_FORMATION) FROM TYPE_FORMATION_FORMATEUR WHERE ID_FORMATEUR=' . $id_formateur . ' AND ID_TYPE_FORMATION=' . $idFormation . '');
+						$valeurNbDates_fin = $requeteNbDates_fin->fetch();
+						$nbDates_fin = $valeurNbDates_fin['COUNT(DATE_FIN_FORMATION)'];
+						echo "<p>$nbDates_fin dates de fin : ";
 
-					//Date fin
-					//requête pour avoir le nombre de dates à la fin
-					$requeteNbDates_fin = $db->query('SELECT COUNT(DATE_FIN) FROM STAGIAIRE_FORMATEUR WHERE ID_FORMATEUR=' . $id_formateur . '');
-					$valeurNbDates_fin = $requeteNbDates_fin->fetch();
-					$nbDates_fin = $valeurNbDates_fin['COUNT(DATE_FIN)'];
-					echo "<p>$nbDates_fin dates de fin : ";
-
-						// Affichage des dates début
+						// Affichage des dates de fin
+						echo "<select name='date_fin'>";						
 						// Requête SQL préparé
-						$requeteDates_fin = $db->prepare('SELECT * FROM STAGIAIRE_FORMATEUR WHERE ID_FORMATEUR=' . $id_formateur . '');
+						$requeteDates_fin = $db->prepare('SELECT * FROM STAGIAIRE_FORMATEUR LEFT JOIN TYPE_FORMATION_FORMATEUR ON STAGIAIRE_FORMATEUR.ID_FORMATEUR = TYPE_FORMATION_FORMATEUR.ID_FORMATEUR WHERE ID_TYPE_FORMATION=' . $idFormation . ' AND STAGIAIRE_FORMATEUR.ID_FORMATEUR=' . $id_formateur . '');
 						// On remplie les paramètres
 						$requeteDates_fin->bindParam(':id_formateur', $i, PDO::PARAM_STR, 20);
-						// On l'exécute
+						// On l'éxécute
 						$requeteDates_fin->execute();
 						// On fetch Rigght !
 						$valeurDate_fin = $requeteDates_fin->fetchAll();
-						echo "<select name=\"dates_fin\">";
 						foreach ($valeurDate_fin as $value){
-
+										
 							$date_fin = $value['DATE_FIN'];
-							echo "<option value=\"$date_fin\"> $date_fin </option>"; echo "<br />";
-
+							echo "<option value='$date_fin'> $date_fin</option><br />";			
 						}
-					echo "</select>";
+						echo "</select></p>";
+					}
+			
 		 }
 		 else{
-			if($_POST['envoyer']==('envoyer')) {
+			if($_POST['envoyer']==('envoyerr')) {
 			// appel du traitement
 			// Enregistrement des variables à partir du formulaire
 				$nom = $_POST['nom'];
 				$prenom = $_POST['prenom'];
-				$nationalite = $_POST['nationalite'];
-				$formation = $_POST['formation'];
+				$idNationalite = $_POST['idNationalite'];
+				$idFormation = $_POST['idFormation'];
 				$option = $_POST['option'];
-
+				
 							// Connection a la base
 							try {
 								// Connexion à la base de données
@@ -182,23 +216,23 @@
 							}
 					//lecture du tableau option[]
 					$option = $_POST['option'];
-					$id_formateur = implode(', ',$option);
+					//$id_formateur = implode(', ',$option);
 					//echo '<p>options:<br><br>'.$id_formateur.'</p>';
-
+					
 					// Dates début
 					// Requête pour avoir le nombre de dates de début
 					$requeteNbDates_debut = $db->query('SELECT COUNT(DATE_DEBUT) FROM STAGIAIRE_FORMATEUR WHERE ID_FORMATEUR=' . $id_formateur . '');
 					$valeurNbDates_debut = $requeteNbDates_debut->fetch();
 					$nbDates_debut = $valeurNbDates_debut['COUNT(DATE_DEBUT)'];
 
-						// Affichage des dates début
+						// Affichage des dates début				
 						// Requête SQL préparé
 						$requeteDates_debut = $db->prepare('SELECT * FROM STAGIAIRE_FORMATEUR WHERE ID_FORMATEUR=' . $id_formateur . '');
 						// On remplie les paramètres
 						$requeteDates_debut->bindParam(':id_formateur', $i, PDO::PARAM_STR, 20);
 						// On l'éxécute
 						$requeteDates_debut->execute();
-						// On fetch Right !
+						// On fetch Rigght !
 						$valeurDate_debut = $requeteDates_debut->fetchAll();
 						foreach ($valeurDate_debut as $value){
 							$date_debut = $value['DATE_DEBUT'];
@@ -210,7 +244,7 @@
 					$valeurNbDates_fin = $requeteNbDates_fin->fetch();
 					$nbDates_fin = $valeurNbDates_fin['COUNT(DATE_FIN)'];
 
-						// Affichage des dates début
+						// Affichage des dates début				
 						// Requête SQL préparé
 						$requeteDates_fin = $db->prepare('SELECT * FROM STAGIAIRE_FORMATEUR WHERE ID_FORMATEUR=' . $id_formateur . '');
 						// On remplie les paramètres
@@ -220,10 +254,10 @@
 						// On fetch Rigght !
 						$valeurDate_fin = $requeteDates_fin->fetchAll();
 						foreach ($valeurDate_fin as $value){
-							$date_fin = $value['DATE_FIN'];
-						}
-
-				if ( ($nom!='') && ($prenom!='') && ($nationalite!='') && ($formation!='') && ($id_formateur!='') && ($date_debut!='') && ($date_fin!='') ) {
+							$date_fin = $value['DATE_FIN'];				
+						}						
+					 
+				if ( ($nom!='') && ($prenom!='') && ($idNationalite!='') && ($idFormation!='') && ($id_formateur!='') && ($date_debut!='') && ($date_fin!='') ) {
 
 					try {
 						// Connexion à la base de données
@@ -232,7 +266,7 @@
 						$db->exec("SET CHARACTER SET utf8");
 						echo "<br/>";
 						echo "Connexion à la base réussi. <br/>";
-
+						
 						// On calcul l'Id à mettre au nouveau stagiaire (l'id max précédent + 1)
 						$requeteId = $db->query('SELECT MAX(ID) as IdMAX FROM STAGIAIRE');
 						$valeurId = $requeteId->fetch();
@@ -242,8 +276,8 @@
 						//echo "Elle vaut maintenant : " . $id . "<br />";
 
 						// Requête SQL préparé
-						$requeteInsertion = $db->prepare('INSERT INTO STAGIAIRE (ID, NOM, PRENOM, ID_NATIONALITE, ID_TYPE_FORMATION)
-							VALUES (:id, :nom, :prenom, :nationalite, :formation)');
+						$requeteInsertion = $db->prepare('INSERT INTO STAGIAIRE (ID, NOM, PRENOM, ID_NATIONALITE, ID_TYPE_FORMATION) 
+							VALUES (:id, :nom, :prenom, :idNationalite, :idFormation)');
 						// requête SQL préparé
 						$requeteInsertion2 = $db->prepare('INSERT INTO STAGIAIRE_FORMATEUR (ID, ID_FORMATEUR, DATE_DEBUT, DATE_FIN)
 							VALUES (:id, :id_formateur, :date_debut, :date_fin)');
@@ -251,10 +285,10 @@
 						$requeteInsertion->bindParam(':id', $id, PDO::PARAM_INT, 2);
 						$requeteInsertion->bindParam(':nom', $nom, PDO::PARAM_STR, 20);
 						$requeteInsertion->bindParam(':prenom', $prenom, PDO::PARAM_STR, 20);
-						$requeteInsertion->bindParam(':nationalite', $nationalite, PDO::PARAM_INT, 2);
-						$requeteInsertion->bindParam(':formation', $formation, PDO::PARAM_INT, 2);
+						$requeteInsertion->bindParam(':idNationalite', $idNationalite, PDO::PARAM_INT, 2);
+						$requeteInsertion->bindParam(':idFormation', $idFormation, PDO::PARAM_INT, 2);
 						//execution de la requête 1
-
+						
 						$requeteInsertion->execute();
 						$requeteInsertion2->bindParam(':id', $id, PDO::PARAM_INT, 2);
 						$requeteInsertion2->bindParam(':id_formateur', $id_formateur, PDO::PARAM_STR, 20);
@@ -263,11 +297,11 @@
 
 						//execution requête 2
 						$requeteInsertion2->execute();
-
+						
 						//confirmation
 						echo "Stagiaire ajouté avec succès !";
 
-
+						
 					}
 					catch (PDOException $e) {
 						echo "Erreur : " . $e->getMessage() . "<br/>";
@@ -279,13 +313,13 @@
 				else {
 					//header('Location: assemblage.php?erreur=1');
 				}
-
+    					
 			}
 		}
 		?>
-
+		
 		<br/>
-
+		
 
     	<a href="index.php">Retour au menu</a>
 
@@ -295,8 +329,8 @@
 				if ($_GET['erreur']==1) {
 					echo "<script>alert('Veuillez remplir le formulaire en entier.')</script>";
 				}
-			}
+			}		
 		?>
-
+    
     </body>
 </html>
