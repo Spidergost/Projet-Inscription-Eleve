919dc5bf06eea6190f71d7794d9444c7184bbcdc
Spidergost <thespidergost@gmail.com>
2015-04-23 11:23:45 +0200

    Mise en Prod

--- affichage-formateur.php
+++ affichage-formateur.php
@@ -27,16 +27,22 @@
     	<form method="post" action="traitement-affichage-formateur.php" class="formulaire">
     	    <br />
     	    <b><u>Les formations</u> :</b><br /><br />
+			
     		<?php
 		        // FORMATION
 		    	// Requête pour avoir le nombre de formation
 		    	$requeteNbFormation = $db->query('SELECT COUNT(ID_TYPE_FORMATION) FROM TYPE_FORMATION');
 		        $valeurNbFormation = $requeteNbFormation->fetch();
 		        $nbFormation = $valeurNbFormation['COUNT(ID_TYPE_FORMATION)'];
-		        echo "<p> $nbFormation formation(s) : ";
-
+		        //echo "<p> $nbFormation formation(s) : ";
+				
+				// FORMATEUR
+		    	// Requête pour avoir le nombre de formateur
+		    	$requeteNbFormateur = $db->query('SELECT COUNT(ID_FORMATEUR) FROM FORMATEUR');
+		        $valeurNbFormateur = $requeteNbFormateur->fetch();
+		        $nbFormateur = $valeurNbFormateur['COUNT(ID_FORMATEUR)'];
+				
 		        // Affichage des formations
-		        echo "<select name=\"idFormation\">";
 		        for ($i=1; $i < ($nbFormation+1) ; $i++) {
 		        	// Requête SQL préparé
 		        	$requeteFormation = $db->prepare('SELECT * FROM TYPE_FORMATION WHERE ID_TYPE_FORMATION=:id');
@@ -48,9 +54,53 @@
 		        	$valeurFormation = $requeteFormation->fetch();
 		        	$idFormation = $valeurFormation['ID_TYPE_FORMATION'];
 		        	$libelleFormation = $valeurFormation['LIBELLE'];
-		        	echo "<option value=\"$idFormation\"> $libelleFormation</option>";
-		        }
-		        echo "</select> </p>";
+					echo "<b><u> $libelleFormation </u> :</b><br /><br />";
+					
+					//Nombre de Formateur dans la Formation à $i
+					/*$requeteFormateurNb = $db->prepare('SELECT NOM, PRENOM, DATE_DEBUT_FORMATION, DATE_FIN_FORMATION
+													  FROM FORMATEUR
+													  LEFT JOIN TYPE_FORMATION_FORMATEUR ON FORMATEUR.ID_FORMATEUR = TYPE_FORMATION_FORMATEUR.ID_FORMATEUR
+													  WHERE TYPE_FORMATION_FORMATEUR.ID_TYPE_FORMATION = :id');
+					// On remplie les paramêtres
+					$requeteFormateurNb->bindParam(':id', $i, PDO::PARAM_INT, 2);
+					// On l'éxécute
+					$requeteFormateurNb->execute();
+					$nbFormaSpec = $requeteFormateurNb->rowCount();
+					echo $nbFormaSpec;
+					for ($y = 0; $y < $nbFormaSpec; $y++) {*/
+						$requeteFormateur = $db->prepare('SELECT NOM, PRENOM, DATE_DEBUT_FORMATION, DATE_FIN_FORMATION
+														FROM FORMATEUR
+														LEFT JOIN TYPE_FORMATION_FORMATEUR ON FORMATEUR.ID_FORMATEUR = TYPE_FORMATION_FORMATEUR.ID_FORMATEUR
+														WHERE TYPE_FORMATION_FORMATEUR.ID_TYPE_FORMATION = :id');
+						// On remplie les paramêtres
+						$requeteFormateur->bindParam(':id', $i, PDO::PARAM_INT, 2);
+						// On l'éxécute
+						$requeteFormateur->execute();
+						// On fetch Rigght !
+						$valeurFormateur = $requeteFormateur->fetchAll();
+						$nblignes = $requeteFormateur->rowCount();
+						
+						if ($nblignes != 0) {
+							echo "<table BORDER = \"1\">";
+							
+							echo "<tr><th>NOM</th><th>PRENOM</th><th>DATE DE DEBUT</th><th>DATE DE FIN</th> </tr>";
+							
+							foreach($valeurFormateur as $ligne) {
+									echo "<tr> <td>" . $ligne['NOM'] . "</td>";
+									echo "<td>" . $ligne['PRENOM'] . "</td>";
+									echo "<td>" . $ligne['DATE_DEBUT_FORMATION'] . "</td>";
+									echo "<td>" . $ligne['DATE_FIN_FORMATION'] . "</td> </tr>";
+									echo "<br />";
+									}
+							echo "</table>";
+						}
+						else {
+							echo "Aucune formations disponible";
+							echo "<br />";
+						}
+					//}
+				}
+
     		?>
                  <br />
 				 <b><u>Formateurs par date</u> :</b><br /><br />
